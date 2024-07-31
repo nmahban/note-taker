af8586cd3140a4ee2a1f4465cab198c4d2229cc9
Purple Persia <navid@navids-imac.mynetworksettings.com>
2024-07-31 19:21:02 -0400

    added some starter js/variables

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
