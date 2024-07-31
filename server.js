8ed84686cd027f84b38524348f06a88a11896ab8
Purple Persia <navid@navids-imac.mynetworksettings.com>
2024-07-31 19:34:47 -0400

    middleware

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
