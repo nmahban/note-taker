503eee9011896415dd96f35566b394de0a77148b
Purple Persia <navid@navids-imac.mynetworksettings.com>
2024-08-01 07:50:33 -0400

    api routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const { EagerLoadingError } = require("sequelize");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
@@ -18,4 +19,30 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "indexl.html"));
 });
 
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
 
+app.post("/api/notes", (req, res) => {
+  const newNote = { ...req.body, id: uuidv4() };
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+    fs.writeFile(
+      path.join(__dirname, "db", "db.json"),
+      JSON.stringify(notes, null, 2),
+      (err) => {
+        if (err) throw err;
+        res.json(newNote);
+      }
+    );
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
